91bd4b1e9157862193e6ecadfe2466302d62a835
Jack <not.pennys.code@gmail.com>
2024-02-16 10:55:39 +0200

    Introduce library for JSONL decoding + tests

---
+++ pkg/jsonl/jsonl.go
@@ -0,0 +1,34 @@
+package jsonl
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+)
+
+// NewDecoder returns a jsonl decoder
+// that actually is a simple json.Decoder with a middleware for cleaning up comments
+func NewDecoder(r io.Reader) *json.Decoder {
+	return json.NewDecoder(NewJSONCommentsCleaner(r))
+}
+
+func DecodeAll[T any](r io.Reader) ([]T, error) {
+	decoder := NewDecoder(r)
+
+	results := make([]T, 0)
+	for {
+		var result T
+		err := decoder.Decode(&result)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("failed on decoding a result: %w", err)
+		}
+
+		results = append(results, result)
+	}
+
+	return results, nil
+}
---
+++ pkg/jsonl/jsonl_test.go
@@ -0,0 +1,59 @@
+package jsonl
+
+import (
+	"os"
+	"testing"
+)
+
+func TestJsonlDecoding(t *testing.T) {
+
+	type Obj struct {
+		Name string `json:"name"`
+		Id   struct {
+			Oid string `json:"$oid"`
+		} `json:"_id"`
+		V *struct {
+			Foo string `json:"foo"`
+		} `json:"v,omitempty"`
+		Nested []struct {
+			V string `json:"v"`
+		} `json:"nested,omitempty"`
+	}
+
+	for _, filename := range []string{
+		// JSONL
+		//"with-comments.compact",
+		//"with-comments",
+		//"with-multiline-comments.compact",
+		//"with-multiline-comments",
+		//"without-comments.compact",
+		//"without-comments",
+
+		// Valid JSON:
+		"valid-json.compact",
+	} {
+		file, err := os.Open("testdata/samples." + filename + ".json")
+		if err != nil {
+			t.Errorf("could not read testdata file %s: %s", filename, err)
+		}
+
+		decoded, err := DecodeAll[Obj](file)
+		if err != nil {
+			t.Errorf("could not decode all %s: %s", filename, err)
+		}
+
+		if len(decoded) != 3 {
+			t.Fatalf("len(decoded) must be 3 (%s), got %d", filename, len(decoded))
+		}
+
+		if decoded[0].Name != "Sample 9" {
+			t.Errorf("decoded[0] name expected to be Sample 9 got %s", decoded[0].Name)
+		}
+		if decoded[1].Name != "Sample 8" {
+			t.Errorf("decoded[0] name expected to be Sample 9 got %s", decoded[0].Name)
+		}
+		if decoded[2].Name != "Sample 7" {
+			t.Errorf("decoded[0] name expected to be Sample 9 got %s", decoded[0].Name)
+		}
+	}
+}
